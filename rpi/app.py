834d11f9dd9f646018597af44452a8bba5057cec
Aum Pauskar <aumpauskar111@gmail.com>
2024-05-12 13:29:03 +0530

    Optimized code

--- rpi/app.py
+++ rpi/app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+# global constants
+on_temp = 65
+off_temp = 55
+gpio = 17
+enable = 27
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio, GPIO.OUT)
+GPIO.setup(enable, GPIO.OUT)
+pwm = GPIO.PWM(enable, 100)
+pwm.start(100)
 
 app = Flask(__name__)
 
@@ -12,7 +28,13 @@ def html_page():
 def send():
     number_input = request.form.get('numberInput', '')
     # Use the 'number' variable for further processing
+    rotate_fan(int(number_input))
     return 'Received number: ' + number_input
 
+def rotate_fan(specified_time):
+    GPIO.output(gpio, GPIO.HIGH)
+    time.sleep(specified_time)
+    GPIO.output(gpio, GPIO.LOW)
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
